7b7fa78c4f8dcf2ba2579dd9b58d0503305ac28b
Your Name <you@example.com>
2026-09-23 12:11:50 +0530

    access donr to vikram sir

--- src/access/access.config.ts
+++ src/access/access.config.ts
@@ -1,14 +1,15 @@
 import type { AccessEntry } from "./access.type.js";
 
 export const CEO_ACCESS: AccessEntry[] = [
-  { phone: "7796419792", name: "Pritam", role: "CEO" },
+  { phone: "885065651", name: "Vikram Sir", role: "CEO" },
 ];
 
 export const ADMIN_ACCESS: AccessEntry[] = [
-  { phone: "7620876689", name: "Pritam Jio", role: "ADMIN" },
+  { phone: "9833322844", name: "Pritam Jio", role: "ADMIN" },
   { phone: "7743990982", name: "Juhi", role: "ADMIN" },
   { phone: "9834759005", name: "Vipul", role: "ADMIN" },
   { phone: "7558666122", name: "Harshal Sir", role: "ADMIN" },
+  { phone: "7796419792", name: "Pritam Sarkar", role: "ADMIN" },
 ];
 
 export const ACCESS_GATE_ENV = {
